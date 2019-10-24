2ff0e4acdd3bfecc3b6f4f9c82def401a6c31dd4
brahmitolikonda <brahmaiah.tolikonda@gmail.com>
2019-10-24 09:49:23 +0530

    new added

---
+++ cleartrip/test_cases/test_trains2.py
@@ -0,0 +1,19 @@
+from utils.Browser import *
+from pages.verify_homepage import *
+from pages.navigate_page import *
+from pages.trains import trains_input
+from components.train_data import train_database
+
+
+
+
+def test_trains():
+    
+    driver=Browser_utils().openBrowser()
+    Browser_utils().invokeApp(driver)
+    verifyPage().verifyHome(driver)          #verifyHome of cleartrip       
+    navigate().navigate_trainspage(driver)   #navigate_trainspage()               
+    verifyPage().verifyTrainpage(driver)    #verifyTrainpage() 
+    train_database().train_details(driver)     #searchTrains()
+    verifyPage().verifyProgram(driver)       #verifytrainlist()
+    Browser_utils().closeBrowser(driver)
